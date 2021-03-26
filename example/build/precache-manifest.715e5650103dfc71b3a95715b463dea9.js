ebd53f3b1ac9fe700eee80e2daeb6f0f450cc9ae
devneser <zh_alex@yahoo.com>
2021-03-26 07:34:50 +0400

    Change version

---
+++ example/build/precache-manifest.715e5650103dfc71b3a95715b463dea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcd422740f0c0be24c1e3722880417c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c13de606b808b234930",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "4ecb5163fe716a6a8912",
+    "url": "./static/js/2.4321f2dc.chunk.js"
+  },
+  {
+    "revision": "26ea6ac29495713065d05ead3530e733",
+    "url": "./static/js/2.4321f2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c13de606b808b234930",
+    "url": "./static/js/main.c1ae3f3c.chunk.js"
+  },
+  {
+    "revision": "bb4719da6b52e1d0e49f",
+    "url": "./static/js/runtime-main.c6c4d617.js"
+  }
+]);
